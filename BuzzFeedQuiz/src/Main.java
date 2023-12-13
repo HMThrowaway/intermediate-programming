6ad958d962f3c83de95895226495694d7c49f671
HMThrowaway <144482894+HMThrowaway@users.noreply.github.com>
2023-12-13 14:25:24 -0500

    Add files via upload

--- BuzzFeedQuiz/src/Main.java
+++ BuzzFeedQuiz/src/Main.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
+
 public class Main {
+    static ArrayList<Question> questions = new ArrayList<>();
     static int[] scores = new int[4];
     static final String[] answers = {
             "Default",
@@ -9,13 +12,42 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("What fortnite skin are you?\n");
 
-        new Question("What was your favorite drop spot?","Junk Junction","Tilted Towers","The Volcano","Salty Springs");
-        new Question("What was your favorite OG gun","Pistol","Pump","Boom Bow","Double Barrel");
-        new Question("What was your favorite season","Season 1","Season 4","Season 8","Season 7");
-        new Question("What was your favorite live event","The Rocket Launch","Meteor Event","Volcano Eruption","Marshmello Concert");
-        new Question("What was your favorite OG heals","Med Kit","Minis","Chug Splashes","Chug Jug");
-
+        questions.add(new Question("What was your favorite OG drop spot?", "Junk Junction", "Tilted Towers", "The Volcano", "Salty Springs"));
+        questions.add(new Question("What was your favorite OG gun?", "Pistol", "Pump", "Boom Bow", "Double Barrel"));
+        questions.add(new Question("What was your favorite OG season?", "Season 1", "Season 4", "Season 8", "Season 7"));
+        questions.add(new Question("What was your favorite OG live event?", "The Rocket Launch", "Meteor Event", "Volcano Eruption", "Kevin The Cube"));
+        questions.add(new Question("What was your favorite OG heals?", "Med Kit", "Minis", "Chug Splashes", "Chug Jug"));
+        questions.add(new Question("What was your favorite OG vehicle?", "Golf Cart", "Quad Crashers", "Ballers", "Planes"));
+        questions.add(new Question("What was your favorite OG movement item?", "Impulse Grenades", "Grappler", "Flintknock Pistol", "Launch Pads"));
+        questions.add(new Question("What was your favorite OG grenade?","Grenade","Air Strike","Boogie Bomb","CLingers"));
+        for (int i = 0; i < questions.size(); i++){
+            questions.get(i).ask();
+            checkUserInput(i);
+        }
         checkAnswer();
+
+    }
+    static void checkUserInput(int i){
+        switch (questions.get(i).ask().toUpperCase()){
+            case "A" -> {
+                Main.scores[0] += 1;
+                return;
+            }
+            case "B" -> {
+                Main.scores[1] += 1;
+                return;
+            }
+            case "C" -> {
+                Main.scores[2] += 1;
+                return;
+            }
+            case "D" -> {
+                Main.scores[3] += 1;
+                return;
+            }
+        }
+        System.out.println("Please enter a valid response");
+        checkUserInput(i);
     }
     static void checkAnswer(){
         int max = 0;
--- BuzzFeedQuiz/src/Question.java
+++ BuzzFeedQuiz/src/Question.java
@@ -14,39 +14,15 @@ public class Question {
         this.a2 = a2;
         this.a3 = a3;
         this.a4 = a4;
-        this.ask();
     }
-    void ask(){
+    String ask(){
         System.out.println(question);
         System.out.println("A: "+a1);
         System.out.println("B: "+a2);
         System.out.println("C: "+a3);
         System.out.println("D: "+a4);
         input = scanner.next();
-        this.checkAnswer();
+        return input;
+    }
 
-    }
-    void checkAnswer(){
-        input = input.toUpperCase();
-        switch (input){
-            case "A" -> {
-                Main.scores[0] += 1;
-                return;
-            }
-            case "B" -> {
-                Main.scores[1] += 1;
-                return;
-            }
-            case "C" -> {
-                Main.scores[2] += 1;
-                return;
-            }
-            case "D" -> {
-                Main.scores[3] += 1;
-                return;
-            }
-        }
-        System.out.println("Please enter a valid response");
-        this.ask();
-    }
 }
