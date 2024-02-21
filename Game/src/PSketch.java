78ee5d3e3b810f12cfa6929d8db3bf3f15592cd9
HMThrowaway <144482894+HMThrowaway@users.noreply.github.com>
2024-02-21 14:27:24 -0500

    Add files via upload

---
+++ Game/src/PSketch.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+import processing.core.PApplet; // java libraries 
+
+public class PSketch extends PApplet {
+
+    public static void main(String[] args) {
+        PApplet.main("PSketch");
+    }
+    @Override
+    public void settings() {
+        size(500, 500);
+    }
+
+    @Override
+    public void draw() {
+        background(255);
+    }
+
+    @Override
+    public void mousePressed() {
+
+    }
+}
